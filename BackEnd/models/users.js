aea993a6c90678ca8c2321e6f001a21b3806035e
t0nxx <mahmoudko1500@hotmail.com>
2018-08-18 23:07:53 +0200

    Update users.js

--- BackEnd/models/users.js
+++ BackEnd/models/users.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
@@ -35,42 +34,4 @@ const Users = mongoose.model('Users', users_schema);
 users_schema.plugin(AutoIncrement, { inc_field: 'user_id' });
 
 
-=======
-
-const mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
-
-const users_schema = new mongoose.Schema({
-    user_id: Number,
-    name: {
-        type: String ,
-        minlength : 3 ,
-        maxlength : 50 ,
-        required : true
-    },
-    email: {
-        type: String,
-        minlength: 5,
-        maxlength: 255,
-        required: true ,
-        unique : true
-    },
-    password: {
-        type: String,
-        minlength: 5,
-        maxlength: 1024,
-        required: true ,
-    },
-    utype_id: {
-        type: Number ,
-        required: true ,
-        ref: 'Users_types'
-    }
-});
-
-const Users = mongoose.model('Users', users_schema);
-users_schema.plugin(AutoIncrement, { inc_field: 'user_id' });
-
-
->>>>>>> 2e070309d6437f260d7d88a4fbdb6df3b75f1c26
 module.exports = Users ;
