e24c33aecb0ced3520c1912a59c26050463c28c8
t0nxx <mahmoudko1500@hotmail.com>
2018-08-18 23:08:37 +0200

    Update userstypes.js

--- BackEnd/models/userstypes.js
+++ BackEnd/models/userstypes.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const mongoose = require('mongoose');
 const AutoIncrement = require('mongoose-sequence')(mongoose);
@@ -23,30 +22,4 @@ users_types_schema.plugin(AutoIncrement, { inc_field: 'utype_id' })
 
 
 
-=======
-
-const mongoose = require('mongoose');
-const AutoIncrement = require('mongoose-sequence')(mongoose);
-
-const users_types_schema = new mongoose.Schema({
-    utype_id: {
-        type: Number ,
-        required : true ,
-    },
-    utype_name: {
-        type : String ,
-        required : true 
-    },
-    users : [{
-        type : mongoose.SchemaTypes.Object ,
-        ref : 'Users'
-    }]
-});
-
-const Users_types = mongoose.model('Users_types', users_types_schema);
-users_types_schema.plugin(AutoIncrement, { inc_field: 'utype_id' })
-
-
-
->>>>>>> 2e070309d6437f260d7d88a4fbdb6df3b75f1c26
 module.exports = Users_types ;
