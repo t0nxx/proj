6b657466ec2157e515a7a127a0ac514a77f2051b
t0nxx <mahmoudko1500@hotmail.com>
2018-08-18 23:02:24 +0200

    u

--- BackEnd/routes/prodtypes.js
+++ BackEnd/routes/prodtypes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -66,73 +66,4 @@ router.delete('/delete', Auth,async (req, res) => {
 
 
 
-=======
-const express = require("express");
-const router = express.Router();
-const mongoose = require('mongoose');
-const Products_types = require('../models/prodtypes');
-const Auth = require('../middlewars/auth');
-
-
-
-
-
-router.get('/', Auth,async (req, res) => {
-    const result = await Products_types
-        .find();
-    res.send(result);
-})
-
-router.get('/', Auth,async (req, res) => {
-    const query = { ptype_id: req.body.ptype_id };
-    const result = await Products_types
-        .find(query);
-    res.send(result);
-})
-
-router.post('/add', Auth,async (req, res) => {
-    const product = new Products_types({
-        ptype_name: req.body.ptype_name
-    });
-    try {
-        await product.save();
-        res.json("added");
-    } catch (error) {
-        res.send(error.message);
-    }
-})
-
-router.put('/update', Auth,async (req, res) => {
-    const updated = req.body;
-    const query = { ptype_id: req.body.ptype_id }
-    try {
-        await Products_types.update(query, updated);
-        res.send("updated");
-    } catch (error) {
-        res.send(error.message);
-    }
-
-})
-
-
-router.delete('/delete', Auth,async (req, res) => {
-    const query = { ptype_id: req.body.ptype_id };
-    try {
-        await Products_types.remove(query);
-        res.send("removed");
-    } catch (error) {
-        res.send(error.message);
-    }
-
-})
-
-
-
-
-
-
-
-
-
->>>>>>> 2e070309d6437f260d7d88a4fbdb6df3b75f1c26
 module.exports = router;
--- BackEnd/routes/userstypes.js
+++ BackEnd/routes/userstypes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const express = require("express");
 const router = express.Router();
 const mongoose = require('mongoose');
@@ -60,84 +60,4 @@ router.delete('/delete', Auth,async (req, res) => {
 
 })
 
-
-
-
-
-
-
-
-
-
-
-=======
-const express = require("express");
-const router = express.Router();
-const mongoose = require('mongoose');
-const Users_types = require('../models/userstypes');
-const Auth = require('../middlewars/auth');
-
-
-
-router.get('/', Auth,async (req, res) => {
-    const result = await Users_types
-        .find();
-    res.send(result);
-})
-
-router.get('/', Auth,async (req, res) => {
-    const query = { utype_id: req.body.utype_id };
-    const result = await Users_types
-        .find(query);
-    res.send(result);
-})
-
-router.post('/add', Auth,async (req, res) => {
-    const users_type = new Users_types({
-        utype_name : req.body.utype_name ,
-        utype_id : req.body.utype_id
-    });
-    try {
-        await users_type.save();
-        res.json("added");
-    } catch (error) {
-        res.send(error.message);
-    }
-})
-
-router.put('/update', Auth,async (req, res) => {
-    const updated = req.body;
-    const query = { utype_id: req.body.utype_id }
-    try {
-        await Users_types.update(query, updated);
-        res.send("updated");
-    } catch (error) {
-        res.send(error.message);
-    }
-
-})
-
-
-router.delete('/delete', Auth,async (req, res) => {
-    const query = { utype_id: req.body.utype_id };
-    try {
-        await Users_types.remove(query);
-        res.send("removed");
-    } catch (error) {
-        res.send(error.message);
-    }
-
-})
-
-
-
-
-
-
-
-
-
-
-
->>>>>>> 2e070309d6437f260d7d88a4fbdb6df3b75f1c26
 module.exports = router;
