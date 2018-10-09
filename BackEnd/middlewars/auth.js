fd58f659f5cbcd44a1e809545935fe9f1dc8b22f
t0nxx <mahmoudko1500@hotmail.com>
2018-10-09 22:10:22 +0200

    fix auth

--- BackEnd/middlewars/auth.js
+++ BackEnd/middlewars/auth.js
@@ -1,14 +1,14 @@
-const jwt = require('jsonwebtoken');
-require('dotenv/config');
-module.exports =(req,res,next) =>{
-    const token = req.header('auth-token');
-    if (!token) return res.status(401).send('access denied no token');
-    try {
-
-        decode = jwt.verify(token, process.env.JWT_KEY)
-        req.user = decode ;
-        next();
-    } catch (error) {
-        res.status(400).send('invalid token');
-    }
-}
+const jwt = require("jsonwebtoken");
+require("dotenv/config");
+module.exports = (req, res, next) => {
+  if (!process.env.RequireAuth) return next();
+  const token = req.header("auth-token");
+  if (!token) return res.status(401).send("access denied no token");
+  try {
+    decode = jwt.verify(token, process.env.JWT_KEY);
+    req.user = decode;
+    next();
+  } catch (error) {
+    res.status(400).send("invalid token");
+  }
+};
